df96ec79c5d84cece04f2414a6ff5963797480f6
AbiSayuti <32661598+AbiSayuti@users.noreply.github.com>
2017-10-10 11:42:13 +0700

    Add navigation and cjange navigation item

--- Costumer Nav bar/Costumer Nav bar/ViewController.swift
+++ Costumer Nav bar/Costumer Nav bar/ViewController.swift
@@ -20,6 +20,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //add function did appear
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        //deklarasi nav sbg nav bar
+        let bnav = self.navigationController?.navigationBar
+        
+        //tambahkan style bar
+        
+        bnav?.barStyle = UIBarStyle.black
+        bnav?.tintColor = UIColor.yellow
+        
+        // menambahkan gambar atau logo
+        //mengatur ukuran logo 40px 40px
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width : 40, height: 40))
+        imageView.contentMode = .scaleAspectFit
+        
+        //memanggil nama gambarnya
+        let image = UIImage(named: "ABIword02.jpg")
+        //menampilkan image ke image view
+        imageView.image = image
+        
+        //menampilkan tittle nav item sbg image view
+        
+        navigationItem.titleView = imageView
+        
+    }
 
 }
 
